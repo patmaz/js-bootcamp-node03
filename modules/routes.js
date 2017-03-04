6b731f8a91e5317d4b0bb534ab06f79369b06198
Patryk Mazurkiewicz <patrykmazurkiewicz@MacBook-Pro-Patryk.local>
2017-03-04 07:57:34 +0100

    no try catch

--- modules/routes.js
+++ modules/routes.js
@@ -7,18 +7,14 @@ var ejs = require('ejs');
 
 function start(req, res){
     console.log('Welcome request'.blue);
-    try {
-        fs.readFile('./templates/start.html', 'utf-8', function(err, data){
-            if (err) throw err;
-            var renderedHtml = ejs.render(data);
+    fs.readFile('./templates/start.html', 'utf-8', function(err, data){
+        if (err) console.error(err);
+        var renderedHtml = ejs.render(data);
 
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.write(renderedHtml);
-            res.end();
-        });
-    } catch(err) {
-        console.error(err);
-    }
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write(renderedHtml);
+        res.end();
+    });
 }
 
 function upload(req, res){
@@ -39,18 +35,14 @@ function upload(req, res){
             console.log('file uploaded: ', file.name);
         })
         .on('end', function(){
-            try {
-                fs.readFile('./templates/uploaded.html', 'utf-8', function(err, data){
-                    if (err) throw err;
-                    var renderedHtml = ejs.render(data, {image: newName});
+            fs.readFile('./templates/uploaded.html', 'utf-8', function(err, data){
+                if (err) console.error(err);
+                var renderedHtml = ejs.render(data, {image: newName});
 
-                    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-                    res.write(renderedHtml);
-                    res.end();
-                });
-            } catch(err) {
-                console.error(err);
-            }
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                res.write(renderedHtml);
+                res.end();
+            });
         });
     form.parse(req);
 }
@@ -58,62 +50,46 @@ function upload(req, res){
 function show(req, res){
     var imgPath = './uploads/' + url.parse(req.url).query.slice(4);
     console.log('Show request: '.blue, imgPath);
-    try {
-        fs.readFile(imgPath, function(err, data){
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'image/gif'});
-            res.write(data, 'binary');
-            res.end();
-        });
-    } catch(err) {
-        console.error(err);
-    }
+    fs.readFile(imgPath, function(err, data){
+        if (err) console.error(err);
+        res.writeHead(200, {'Content-Type': 'image/gif'});
+        res.write(data, 'binary');
+        res.end();
+    });
 }
 
 function chat(req, res){
     console.log('Chat request'.blue);
-    try {
-        fs.readFile('./templates/chat.html', 'utf-8', function(err, data){
-            if (err) throw err;
-            var renderedHtml = ejs.render(data);
+    fs.readFile('./templates/chat.html', 'utf-8', function(err, data){
+        if (err) console.error(err);
+        var renderedHtml = ejs.render(data);
 
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.write(renderedHtml);
-            res.end();
-        });
-    } catch(err) {
-        console.error(err);
-    }
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write(renderedHtml);
+        res.end();
+    });
 }
 
 function js(req, res){
     var jsPath = './templates/js/' + url.parse(req.url).query.slice(5);
     console.log('JS request: '.blue, jsPath);
-    try {
-        fs.readFile(jsPath, 'utf-8', function(err, data){
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf-8'});
-            res.write(data);
-            res.end();
-        });
-    } catch(err) {
-        console.error(err);
-    }
+    fs.readFile(jsPath, 'utf-8', function(err, data){
+        if (err) console.error(err);
+        res.writeHead(200, {'Content-Type': 'text/javascript; charset=utf-8'});
+        res.write(data);
+        res.end();
+    });
 }
 
 function css(req, res){
     var cssPath = './templates/css/' + url.parse(req.url).query.slice(5);
     console.log('CSS request: '.blue, cssPath);
-    try {
-        fs.readFile(cssPath, 'utf-8', function(err, data){
-            if (err) throw err;
-            res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
-            res.write(data);
-            res.end();
-        });
-    } catch(err) {
-        console.error(err);
-    }
+    fs.readFile(cssPath, 'utf-8', function(err, data){
+        if (err) console.error(err);
+        res.writeHead(200, {'Content-Type': 'text/css; charset=utf-8'});
+        res.write(data);
+        res.end();
+    });
 }
 
 function error(req, res){
